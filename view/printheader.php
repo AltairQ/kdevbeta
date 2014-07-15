f4b31acc7483d6f1b25ad4e2bfc0e0be06909654
Altair <maciejkamat@gmail.com>
2014-07-15 17:19:46 +0200

    print test

--- view/printheader.php
+++ view/printheader.php
@@ -2,7 +2,7 @@
 
 function printheader($title='knet')
 {
-$hdr= file_get_contents("../templates/header.cpl");
+$hdr= file_get_contents("templates/header.cpl");
 echo str_replace("{{title}}", $title, $hdr);
 }
 
