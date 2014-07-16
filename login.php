22553dd9ebab55ca04e87249f52734b2c7857356
Altair <maciejkamat@gmail.com>
2014-07-16 12:48:30 +0200

    login ;__ ;
    how do i css

--- login.php
+++ login.php
@@ -1,4 +1,60 @@
 <?php
 
+require_once 'view/printheader.php';
+printheader();
+printnavbar();
+
+?>
+<div class="container" style= "margin-top:30px">
+<div class="jumbotron" style="float:none; margin:0 auto;">
+<div style="margin: 0 auto; width:80%;">
+	<form class="form-horizontal">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Log in to proceed</legend>
+
+<!-- Text input-->
+<div class="control-group">
+  <label class="control-label" for="login"></label>
+  <div class="controls">
+    <input id="login" name="login" placeholder="Login" class="input-large" required="" type="text">
+    
+  </div>
+</div>
+
+<!-- Password input-->
+<div class="control-group">
+  <label class="control-label" for="password"></label>
+  <div class="controls">
+    <input id="password" name="password" placeholder="Password" class="input-large" required="" type="password">
+    
+  </div>
+</div>
+
+<!-- Button -->
+<div class="control-group">
+  <label class="control-label" for="apply"></label>
+  <div class="controls">
+    <button id="apply" name="apply" class="btn btn-primary">Log in</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+</div>
+
+	
+</div>
+
+
+</div>
+
+<?php
+
+
+printjsend();
+
+
 	
 ?>
--- view/printheader.php
+++ view/printheader.php
@@ -1,10 +1,22 @@
 <?php
 
-function printheader($title='knet')
+function printheader($title='KNet')
 {
 $hdr= file_get_contents("templates/header.cpl");
 echo str_replace("{{title}}", $title, $hdr);
 }
+
+function printjsend()
+{
+	echo file_get_contents("templates/jsend.cpl");
+}
+
+function printnavbar()
+{
+	$navbar = file_get_contents("templates/navbar.cpl");
+	echo $navbar;
+}
+
 
 
 ?>
