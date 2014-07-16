0db7106b267589b996c24bde8d92f72924cee14a
Altair <maciejkamat@gmail.com>
2014-07-16 11:14:17 +0200

    poprawka do crypt.php

--- control/crypt.php
+++ control/crypt.php
@@ -5,14 +5,14 @@
 	
 
 	//generowanie blowfisha z losowym saltem
-	function generate_hash($input, $rounds = 7)
+	function generate_hash($input, $rounds = 10)
 	{
 		$salt = "";
 		$salt_chars = array_merge(range('A','Z'), range('a','z'), range(0,9));
 		for($i=0;$i < 22;$i++){
 		$salt .= $salt_chars[array_rand($salt_chars)];
 		} 
-		return crypt($input, sprintf('$2a$%02d$', $rounds) . $salt);
+		return crypt($input, sprintf('$2y$%02d$', $rounds) . $salt);
 	}
 
 	//sprawdzenie hashy - hash jest od razu saltem
