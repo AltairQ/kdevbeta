904380be1af36ba962ce8c557359c81c004858c8
Altair <maciejkamat@gmail.com>
2014-07-16 10:45:42 +0200

    kryptografia ftw

---
+++ control/crypt.php
@@ -0,0 +1,24 @@
+<?php
+ // Original PHP code by Chirp Internet: www.chirp.com.au 
+ // Please acknowledge use of this code by including this header.
+ // bla bla bla
+	
+
+	//generowanie blowfisha z losowym saltem
+	function generate_hash($input, $rounds = 7)
+	{
+		$salt = "";
+		$salt_chars = array_merge(range('A','Z'), range('a','z'), range(0,9));
+		for($i=0;$i < 22;$i++){
+		$salt .= $salt_chars[array_rand($salt_chars)];
+		} 
+		return crypt($input, sprintf('$2a$%02d$', $rounds) . $salt);
+	}
+
+	//sprawdzenie hashy - hash jest od razu saltem
+	function check_hash($pass, $hash)
+	{
+		return (crypt($pass, $hash)===$hash);
+	}
+
+?>
