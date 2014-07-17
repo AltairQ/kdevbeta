4b65a59845a9296ef7d968c42addc10e3d661fcb
rufus44 <szubinskik@gmail.com>
2014-07-17 13:32:14 +0200

    Poprawka walidacji
    Zapewne jedna z wielu

--- control/validate.php
+++ control/validate.php
@@ -4,8 +4,10 @@
 function validate($data)
 {
 	if(!($data['username']&&$data['password'])) return null;
-	$username = filter_var($data['username'], FILTER_SANITIZE_SPECIAL_CHAR);
+	$username = filter_var($data['username'], FILTER_SANITIZE_ENCODED);
+	trim($username);
 	$password = filter_var($data['password'], FILTER_SANITIZE_EMAIL);
+	trim($password);
 	return array('username'=>$username, 'password' => $password);
 }
 ?>
