79790ab1fc7a67fa022fd19ea884569cbb748449
Altair <maciejkamat@gmail.com>
2014-07-23 14:45:36 +0200

    jakieśtam obliczenia do SM2
    na podstawie: http://www.supermemo.com/english/ol/sm2.htm

---
+++ model/sm2.php
@@ -0,0 +1,27 @@
+<?php
+
+public function newEF($EF, $q)
+{
+	$nEF= $EF - 0.8 + 0.28*$q - 0.02*$q*$q;
+	if ($nEF < 1.3) {
+		$nEF = 1.3;
+	}
+
+	return $nEF;
+}
+
+public function newI($i, $EF, $rno)
+{
+	if ($rno == 1) {
+		return 1;
+	}
+
+	if ($rno == 2) {
+		return 6;
+	}
+	
+	return $i*$EF;
+}
+
+
+?>
