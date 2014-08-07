1c2ed717faa8b8a3b0ad0d886259461c14c8c30a
rufus44 <szubinskik@gmail.com>
2014-08-07 15:21:18 +0200

    PS do tworzenia listy
    Usunąłem sprawdzanie rowCountem, bo dane zawsze są niepowtarzalne, a
    wyjątki nie śpi�

--- model/create_list.php
+++ model/create_list.php
@@ -2,22 +2,27 @@
 
 function createList($db, $userid)
 {
-	$insert_query = "INSERT INTO `lists` (name, creator, creation_date, edit_date) VALUES('New list', :userid, :creation_date, :edit_date);";
+	$lists_query = "INSERT INTO `lists` (name, creator, creation_date, edit_date) VALUES('New list', :userid, :creation_date, :edit_date);";
+	$list2user_query = "INSERT INTO `list2user` (list_id, user_id, permission) VALUES(:list_id, :user_id, :permission);";
 	try
 	{
-		$statement = $db->prepare($insert_query, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		//dodaj do 'lists'
+		$statement = $db->prepare($lists_query, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 		$statement -> execute(array(':userid'=>$userid, ':creation_date' => date("Y-m-d H:i:s"), ":edit_date" => date("Y-m-d H:i:s")));
-		$create_query = "CREATE TABLE  `l".$db->lastInsertId('id')."` ( `id` INT UNSIGNED NOT NULL AUTO_INCREMENT , `front` TEXT , `back` TEXT , `comment` TEXT , PRIMARY KEY  (`id`) , UNIQUE  (`id`) )";
-		if($statement->rowCount()==1)
-			return 1;
-		else
-			return 0;
+		//pobierz id listy
+		$listid = $db->lastInsertId('id');
+		//stwórz tabelę
+		$create_query = "CREATE TABLE  `l$listid` ( `id` INT UNSIGNED NOT NULL AUTO_INCREMENT , `front` TEXT , `back` TEXT , `comment` TEXT , PRIMARY KEY  (`id`) , UNIQUE  (`id`) )";
 		$db -> exec($create_query);
+		//dodaj do 'list2user'
+		$statement = $db->prepare($list2user_query, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		$statement -> execute(array(':list_id'=>$listid, ':user_id' => $userid, ":permission" => 1));
 	}
 	catch(Exception $e)
 	{
 		echo $e->getMessage();
 		return 0;
 	}
+	return 1;
 }
 ?>
