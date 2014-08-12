fb5b102bbfe46a7c9901031b21e8284c83fa6996
rufus44 <szubinskik@gmail.com>
2014-08-12 23:49:35 +0200

    'r' bez dodawania nowych list
    SQL - kocham, lubię, szanuję

--- model/update_recalled_words.php
+++ model/update_recalled_words.php
@@ -2,10 +2,10 @@
 
 function updateRecalledWords($db, $userid)
 {
-	$lists_query = "SELECT DISTINCT `list_id` FROM `list2user` WHERE `user_id` IN (SELECT DISTINCT `group_id` FROM user2group WHERE `user_id` = $userid AND `permission`>=1) OR `user_id` = $userid;";
+	$lists_query = "SELECT DISTINCT `list_id` FROM `list2user` WHERE `user_id` IN (SELECT DISTINCT `group_id` FROM user2group WHERE `user_id` = $userid AND `permission`>=1) OR (`user_id` = $userid AND `permission` >=1);";
 	$delete_lists_query = "DELETE FROM `r$userid` WHERE `list_id` NOT IN ";
-	$delete_words_query = "DELETE FROM `r$userid` WHERE `word_id` NOT IN (SELECT `id` FROM `l:list_id`) AND `list_id` = :list_id";
-	$insert_words_query = "INSERT INTO `r$userid` (list_id, word_id, ef, repid, replen, repnext) SELECT :list_id, `id`, 1, 1 ,1, '1970-01-01' FROM `l:list_id` WHERE `id` NOT IN (SELECT `word_id` FROM `r$userid`)";
+	$delete_words_temp = "DELETE FROM `r$userid` WHERE `word_id` NOT IN (SELECT `id` FROM `l:list_id`) AND `list_id` = :list_id";
+	$insert_words_temp = "INSERT INTO `r$userid` (list_id, word_id, ef, repid, replen, repnext) SELECT :list_id, `id`, 1, 1 ,1, '1970-01-01' FROM `l:list_id` WHERE `id` NOT IN (SELECT `word_id` FROM `r$userid` WHERE `list_id`=:list_id) AND :list_id IN (SELECT `list_id` FROM `r$userid`)";
 	try
 	{
 		$statement = $db->query($lists_query);
@@ -14,8 +14,8 @@ function updateRecalledWords($db, $userid)
 		$statement = $db->query($delete_lists_query);
 		foreach($lists as $list)
 		{
-			$delete_words_query = str_replace(":list_id", $list, $delete_words_query);
-			$insert_words_query = str_replace(":list_id", $list, $insert_words_query);
+			$delete_words_query = str_replace(":list_id", $list, $delete_words_temp);
+			$insert_words_query = str_replace(":list_id", $list, $insert_words_temp);
 			$statement = $db->query($delete_words_query);
 			$statement = $db->query($insert_words_query);
 		}
