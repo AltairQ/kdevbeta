f83d3df2fa2df7035798e65f76024b03f5959990
rufus44 <szubinskik@gmail.com>
2014-08-13 23:04:23 +0200

    Dodawanie listy do rep

---
+++ model/add_list_to_rep.php
@@ -0,0 +1,17 @@
+<?php
+
+function addListToRep($db, $listid, $userid)
+{
+	$insert_query = "INSERT INTO `r$userid` (list_id, word_id, ef, repid, replen, repnext) SELECT $listid, `id`, 1, 1 ,1, '1970-01-01' FROM `l$listid` WHERE `id` NOT IN (SELECT `word_id` FROM `r$userid` WHERE `list_id`=$listid)";
+	try
+	{
+		$statement = $db->query($insert_query);
+		return true;
+	}
+	catch(Exception $e)
+	{
+		echo $e->getMessage();
+		return -100;
+	}
+}
+?>
